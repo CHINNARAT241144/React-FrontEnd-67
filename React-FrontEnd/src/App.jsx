d01a686cfc5cb8284f1b5cf0edbdd989f7628e11
CHINNARAT241144 <dew.eyecandy@gmail.com>
2024-09-18 10:23:26 +0700

    9/18/2024

---
+++ React-FrontEnd/src/App.jsx
@@ -0,0 +1,24 @@
+import Hello from './assets/Hello';
+import Contact from './assets/Contact';
+import Counter from './assets/Counter';
+
+function App() {
+  const helloData = [
+    {name: 'Anirach', message: 'Hi there'},
+    {name: 'Dew', message: 'Hello..'}
+  ];
+
+  return (
+  <div>
+    <Counter/>
+    {helloData.map((data, index)=> (
+      <Hello key={index} name={data.name} message={data.message} />
+    ))}
+  
+    <Contact email="Anirach@gmail.com" phone ="0817320731"/>
+  </div>
+  );
+}
+
+export default App;
+
---
+++ React-FrontEnd/src/assets/Hello.jsx
@@ -0,0 +1,20 @@
+
+const Hello = ({ message, name }) => {
+    console.log( { message, name });
+    return (
+        <div>
+            <h1>
+                {message} {name}
+            </h1>
+        </div>
+    );
+};
+
+import PropTypes from 'prop-types';
+
+Hello.protoType ={
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+};
+
+export default Hello;
